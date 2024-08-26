5c322b60dc3950c079a1295fc6d0358f020082f4
Gary McGovern <gazzaplays@gmail.com>
2024-08-26 19:11:38 +0200

    Updated for SFSE 0.2.12 (#23)
    * Bump script version
    
    * Updated script for sfse 0.2.12
    
    Added fix for env environment when installing choco
    
    * Added option to uninstall choco packages
    
    Added force tag to choco install to ensure packages install correctly
    
    * Clean-up

--- scripts/cli.ps1
+++ scripts/cli.ps1
@@ -123,6 +123,11 @@ Do {
                         setPythonChoice
                         break
                     }
+                    4 { 
+                        Clear-Host
+                        installProg "Uninstall"
+                        break
+                    }
                     Default {
                     }
                 }
@@ -145,6 +150,7 @@ Do {
                     1. Set Paths
                     2. Set Bypass Choice
                     3. Set Python Choice
+                    4. Uninstall Dependencies (Choco Packages only)
                     q. Return
                 "
                 $setConfigOption = Read-Host "Choose an option"
